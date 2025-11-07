64a707443bc75908edfd07b83c57360b78bd7eac
Renzo Rodriguez <r.nrf@runbox.com>
2025-11-07 14:32:05 +0100

    Start commit

---
+++ AdventOfCode/2015_Day2.cs
@@ -0,0 +1,15 @@
+using System;
+namespace AdventOfCode
+{
+    public class Day2
+    {
+        public static void WrapingPaper(string input)
+        {
+            int lenght = 0;
+            int width = 0;
+            int height = 0;
+
+            Console.WriteLine(input[0]);
+        }
+    }
+}
---
+++ AdventOfCode/MerryCoding.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode
+{
+    public class MerryCoding
+    {
+        static void Main(string[] args)
+        {
+            Day1.CountFloors(Console.ReadLine());
+        }
+    }
+}
